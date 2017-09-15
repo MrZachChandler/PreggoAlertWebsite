43a6d6588ecc0a83a4c452679ef2740e3489047d
Zach Chandler <chandler@Router.local>
2017-09-15 08:17:39 -0500

    bug fixes

--- js/custom.js
+++ js/custom.js
@@ -11,16 +11,16 @@ jQuery(document).ready(function( $ ) {
     separator: ",",
     speed: 3000
   });
-  
+
   // Initiate the wowjs
   new WOW().init();
-  
+
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {opacity:'show'},
     speed: 400
   });
-  
+
   // Mobile Navigation
   if( $('#nav-menu-container').length ) {
       var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
@@ -29,19 +29,19 @@ jQuery(document).ready(function( $ ) {
       $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
       $('body').append( '<div id="mobile-body-overly"></div>' );
       $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-      
+
       $(document).on('click', '.menu-has-children i', function(e){
           $(this).next().toggleClass('menu-item-active');
           $(this).nextAll('ul').eq(0).slideToggle();
           $(this).toggleClass("fa-chevron-up fa-chevron-down");
       });
-      
+
       $(document).on('click', '#mobile-nav-toggle', function(e){
           $('body').toggleClass('mobile-nav-active');
           $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
           $('#mobile-body-overly').toggle();
       });
-      
+
       $(document).click(function (e) {
           var container = $("#mobile-nav, #mobile-nav-toggle");
           if (!container.is(e.target) && container.has(e.target).length === 0) {
@@ -55,7 +55,7 @@ jQuery(document).ready(function( $ ) {
   } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
       $("#mobile-nav, #mobile-nav-toggle").hide();
   }
-  
+
   // Stick the header at top on scroll
   $("#header").sticky({topSpacing:0, zIndex: '50'});
 
@@ -64,13 +64,13 @@ jQuery(document).ready(function( $ ) {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           if (target.length) {
-              
+
               var top_space = 0;
-              
+
               if( $('#header').length ) {
                 top_space = $('#header').outerHeight();
               }
-              
+
               $('html, body').animate({
                   scrollTop: target.offset().top - top_space
               }, 1500, 'easeInOutExpo');
@@ -85,12 +85,12 @@ jQuery(document).ready(function( $ ) {
                   $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
                   $('#mobile-body-overly').fadeOut();
               }
-              
+
               return false;
           }
       }
   });
-  
+
   // Back to top button
   $(window).scroll(function() {
 
@@ -99,12 +99,87 @@ jQuery(document).ready(function( $ ) {
       } else {
           $('.back-to-top').fadeOut('slow');
       }
-      
+
   });
-  
+
   $('.back-to-top').click(function(){
       $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
       return false;
   });
 
+});
+var stripe = Stripe('pk_test_4JnqKnzq03xheOoydf79dvgY');
+var elements = stripe.elements();
+
+var card = elements.create('card', {
+  iconStyle: 'solid',
+  style: {
+    base: {
+      iconColor: '#8898AA',
+      color: 'white',
+      lineHeight: '36px',
+      fontWeight: 300,
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSize: '19px',
+
+      '::placeholder': {
+        color: '#8898AA',
+      },
+    },
+    invalid: {
+      iconColor: '#e85746',
+      color: '#e85746',
+    }
+  },
+  classes: {
+    focus: 'is-focused',
+    empty: 'is-empty',
+  },
+});
+card.mount('#card-element');
+var inputs = document.querySelectorAll('input.field');
+Array.prototype.forEach.call(inputs, function(input) {
+  input.addEventListener('focus', function() {
+    input.classList.add('is-focused');
+  });
+  input.addEventListener('blur', function() {
+    input.classList.remove('is-focused');
+  });
+  input.addEventListener('keyup', function() {
+    if (input.value.length === 0) {
+      input.classList.add('is-empty');
+    } else {
+      input.classList.remove('is-empty');
+    }
+  });
+});
+
+function setOutcome(result) {
+  var successElement = document.querySelector('.success');
+  var errorElement = document.querySelector('.error');
+  successElement.classList.remove('visible');
+  errorElement.classList.remove('visible');
+
+  if (result.token) {
+    // Use the token to create a charge or a customer
+    // https://stripe.com/docs/charges
+    successElement.querySelector('.token').textContent = result.token.id;
+    successElement.classList.add('visible');
+  } else if (result.error) {
+    errorElement.textContent = result.error.message;
+    errorElement.classList.add('visible');
+  }
+}
+
+card.on('change', function(event) {
+  setOutcome(event);
+});
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var form = document.querySelector('form');
+  var extraDetails = {
+    name: form.querySelector('input[name=cardholder-name]').value,
+  };
+  stripe.createToken(card, extraDetails).then(setOutcome);
 });
